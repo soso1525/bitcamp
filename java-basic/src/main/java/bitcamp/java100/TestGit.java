929d92ff0d0ec0dea958cad99f6a7ffbd2143f40
soso1525 <sandy1525@naver.com>
2017-10-11 11:21:02 +0900

    'hi'

--- java-basic/src/main/java/bitcamp/java100/TestGit.java
+++ java-basic/src/main/java/bitcamp/java100/TestGit.java
@@ -2,6 +2,8 @@ package bitcamp.java100;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        String str = "ABCDEFGHIJKLMN";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse().toString());
     }
 }

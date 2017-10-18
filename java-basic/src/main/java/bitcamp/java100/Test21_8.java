1fe52107bc0d4546d404f28a616ef4fba3dded21
soso1525 <sandy1525@naver.com>
2017-10-18 18:16:41 +0900

    'rex'

--- java-basic/src/main/java/bitcamp/java100/Test21_8.java
+++ java-basic/src/main/java/bitcamp/java100/Test21_8.java
@@ -2,28 +2,29 @@ package bitcamp.java100;
 
 import java.io.Console;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test21_8 {
     static class Person {
         String name, email, phone;
 
         void showInfo() {
-            // System.out.println("이름 : " + name);
-            // System.out.println("이메일: " + email);
-            // System.out.println("전화번호: " + phone);
 
             System.out.printf("%-4s\t%-20s\t%-10s\n", name, email, phone);
         }
     }
 
-    static boolean checkEmail(String email) {
-
-        return true;
+    static boolean checkMail(String mail) {
+        Pattern mp = Pattern.compile("[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]+");
+        Matcher m = mp.matcher(mail);
+        return m.matches();
     }
 
     static boolean checkPhone(String phone) {
-
-        return true;
+        Pattern mp = Pattern.compile("\\d{3,4}-\\d{4}");
+        Matcher m = mp.matcher(phone);
+        return m.matches();
     }
 
     public static void main(String[] args) {
@@ -36,7 +37,9 @@ public class Test21_8 {
             Person p = new Person();
             p.name = c.readLine("이름: ");
             p.email = c.readLine("이메일: ");
+//            if(checkMail(p.email)) {}
             p.phone = c.readLine("전화번호: ");
+//            if(checkMail(p.phone)) {}
             a.add(p);
             System.out.println("저장하였습니다");
 
